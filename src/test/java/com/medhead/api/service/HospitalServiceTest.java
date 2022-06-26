1d3c2bcdbaecd07538a6df6e9a095bbd406ffb44
Stiven <stiven.guillaume@gmail.com>
2022-06-26 17:47:11 +0200

    [UPDATE] UT for HospitalService

--- src/test/java/com/medhead/api/service/HospitalServiceTest.java
+++ src/test/java/com/medhead/api/service/HospitalServiceTest.java
@@ -2,6 +2,7 @@ package com.medhead.api.service;
 
 import com.medhead.api.model.Hospital;
 import com.medhead.api.repository.HospitalRepository;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -25,6 +27,8 @@ class HospitalServiceTest {
 
     @Mock
     private HospitalRepository repository;
+    @Mock
+    private GisOperations gisMock;
     @InjectMocks
     private HospitalService service;
     List<Hospital> hospitalList = new ArrayList<Hospital>();
@@ -44,13 +48,13 @@ class HospitalServiceTest {
         hopital2.setName("Princess Grace Hospital");
         hopital3.setName("Spire Tunbridge Wells Hospital");
 
-        hopital1.setLatitude((float)-0.19137082993984222);
-        hopital2.setLatitude((float)-0.15291328728199005);
-        hopital3.setLatitude((float)0.1881597638130188);
+        hopital1.setLongitude((float)-1.21137082993984222);
+        hopital2.setLongitude((float)-0.15291328728199005);
+        hopital3.setLongitude((float)0.1881597638130188);
 
-        hopital1.setLongitude((float)51.486518859863281);
-        hopital2.setLongitude((float)51.495105743408203);
-        hopital3.setLongitude((float)51.134513854980469);
+        hopital1.setLatitude((float)51.766518859863281);
+        hopital2.setLatitude((float)51.495105743408203);
+        hopital3.setLatitude((float)51.134513854980469);
 
         hopital1.setSpecialities("audio vestibular medicine,prosthodontics,endocrinology and diabetes mellitus,restorative dentistry");
         hopital2.setSpecialities("pharmacology, acute internal medicine,oral and maxillo-facial surgery,paediatric dentistry,clinical neurophysiology");
@@ -78,15 +82,44 @@ class HospitalServiceTest {
     }
 
     @Test
-    public void getAll() {
+    public void getAllHospital() {
         //je créé le mock et sa réponse
         when(repository.findAll()).thenReturn(hospitalList);
         //when(repository.findBySpecialities("")).thenReturn(hospitalList);
         //List<Hospital> emptyList = service.findBySpecialities("pharmacology");
         List<Hospital> emptyList = (List)service.getAllHospital();
         //test a revoir
-
         assertEquals(3, emptyList.size());
+    }
+
+    @Test
+    public void getHospital() {
+        //je créé le mock et sa réponse
+        when(repository.findById(1)).thenReturn(Optional.ofNullable((Hospital) hospitalList.get(1)));
+        //when(repository.findBySpecialities("")).thenReturn(hospitalList);
+        Optional<Hospital> testedHospital = service.getHospital(1);
+        //test a revoir
+        assertEquals("Princess Grace Hospital", testedHospital.get().getName());
+    }
+
+    @Test
+    public void findByFreebed() {
+        //je créé le mock et sa réponse
+        when(repository.findByFreebed()).thenReturn(hospitalList);
+        //when(repository.findBySpecialities("")).thenReturn(hospitalList);
+        List<Hospital> emptyList = (List)service.findByFreebed();
+        //test a revoir
+        assertEquals(3, emptyList.size());
+    }
+
+    @Test
+    public void getAllHospitalInRange() {
+        //je créé le mock et sa réponse
+        //when(gisMock.distFrom(-1.80,52,-1.80,51)).thenReturn(37.0);
+        when(repository.findByFreebed()).thenReturn(hospitalList);
+        List<Hospital> emptyList = (List)service.getAllHospitalInRange((float) -1.80, (float) 52.00,50);
+        //test a revoir
+        assertEquals(1, emptyList.size());
     }
 
 }
